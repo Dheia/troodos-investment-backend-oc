799551728db087dd94c6edb3c643a70b778f38c0
CSymeou <c.symeou@blupath.co.uk>
2020-09-24 11:16:48 +0300

    Components cleanup

--- plugins/bl/regionalinvestment/Plugin.php
+++ plugins/bl/regionalinvestment/Plugin.php
@@ -8,16 +8,7 @@ class Plugin extends PluginBase
 {
     public function registerComponents()
     {
-        return [
-            'BL\RegionalInvestment\Components\CommunitiesList' => 'communitiesList',
-            'BL\RegionalInvestment\Components\Featured' => 'featured',
-            'BL\RegionalInvestment\Components\FeaturedOpportunity' => 'featuredOpportunity',
-            'BL\RegionalInvestment\Components\InvestmentSearch' => 'investmentSearch',
-            'BL\RegionalInvestment\Components\OpportunitiesList' => 'opportunitiesList',
-            'BL\RegionalInvestment\Components\PointsOfInterestList' => 'pointsOfInterestList',
-            'BL\RegionalInvestment\Components\RegionsList' => 'regionsList',
-            'BL\RegionalInvestment\Components\SuccessStoriesList' => 'successStoriesList',
-        ];
+        return [];
     }
 
     public function registerSettings()

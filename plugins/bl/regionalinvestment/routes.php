b7d4c3a2c5f29f420348cb87617cb335cceafa93
CSymeou <c.symeou@blupath.co.uk>
2020-09-29 11:08:31 +0300

    Fixed Background

--- plugins/bl/regionalinvestment/routes.php
+++ plugins/bl/regionalinvestment/routes.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 
 App::before(function ($request) {
-  /*
+
   if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == "https") {
     URL::forceScheme("https");
   }
-  */
 
   Route::post('api/favorites/check', 'Bl\RegionalInvestment\Controllers\FavoriteController@apiCheck')->middleware('web');
   Route::post('api/favorites/', 'Bl\RegionalInvestment\Controllers\FavoriteController@apiToggle')->middleware('web');
